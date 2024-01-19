f90e0ca97ccb92693b94da32c0bec918e5eb0d1a
koraykoksal <kksl.koray@gmail.com>
2024-01-19 12:24:23 +0300

    first commit

---
+++ src/configs/dbConnections.js
@@ -0,0 +1,18 @@
+"use strict"
+
+
+
+const mongosee=require('mongoose')
+
+
+const dbConnection=function(){
+
+    mongosee.connect(process.env.MONGODB)
+    .then(()=>console.log('* DB Connected *'))
+    .catch(()=>console.log('*! DB Connection Error !*'))
+}
+
+module.exports={
+    mongosee,
+    dbConnection
+}
