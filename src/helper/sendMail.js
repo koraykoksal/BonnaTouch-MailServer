789c90e97e8995beaad15cc18dc8c62810e1b974
koraykoksal <kksl.koray@gmail.com>
2024-01-21 23:22:05 +0300

    up

--- src/helper/sendMail.js
+++ src/helper/sendMail.js
@@ -2,42 +2,6 @@
 
 const nodemailer = require('nodemailer')
 
-
-// const transporter = nodemailer.createTransport({
-
-//     host: "smtp.gmail.com",
-//     auth: {
-//         user: process.env.MAIL_FROM,
-//         pass: process.env.NODEMAILER_PASS
-//     }
-
-// });
-
-// module.exports = function (data) {
-
-//     const mailOptions = {
-//         from: {
-//             name: "Bonna Touch",
-//             address: process.env.MAIL_FROM,
-//         },
-//         to: data?.to,
-//         subject: 'Bonna Touch Design AI Support Information',
-//         text: `
-//            ${data?.data}
-//         `
-
-//     };
-
-
-//     return transporter.sendMail(mailOptions)
-//         .then(res => console.log(res.response))
-//         .catch(err => console.log(err))
-
-
-// }
-
-
-
 async function sendMail(data) {
     let transporter = nodemailer.createTransport({
         host: "smtp.gmail.com",
